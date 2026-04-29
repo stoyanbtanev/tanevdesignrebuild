eaa15fab5c8646f33480d4e1f78f4dccb036b9c6
Stoyan Tanev <stoyanbtanev@gmail.com>
2026-04-29 06:49:46 +0300

    p

--- components/ui/marquee.tsx
+++ components/ui/marquee.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import type { ComponentPropsWithoutRef, ReactNode } from "react";
+import type { ComponentPropsWithoutRef, CSSProperties, ReactNode } from "react";
 
 interface MarqueeProps extends ComponentPropsWithoutRef<"div"> {
   className?: string;
@@ -19,39 +19,34 @@ export function Marquee({
   vertical = false,
   repeat = 5,
   speed = "normal",
+  style,
   ...props
 }: MarqueeProps) {
   const speedVariants = {
-    slow: "[--duration:120s]",
-    normal: "[--duration:40s]",
-    fast: "[--duration:10s]"
+    slow: "120s",
+    normal: "40s",
+    fast: "10s"
   };
 
   return (
     <div
       {...props}
-      className={cn(
-        "group flex overflow-hidden p-1 [--gap:6px] [gap:var(--gap)]",
-        speedVariants[speed],
+      data-pause-on-hover={pauseOnHover || undefined}
+      data-reverse={reverse || undefined}
+      data-vertical={vertical || undefined}
+      style={
         {
-          "flex-row": !vertical,
-          "flex-col": vertical
-        },
-        className
-      )}
+          ...style,
+          "--duration": speedVariants[speed],
+          "--gap": "6px"
+        } as CSSProperties
+      }
+      className={cn("ui-marquee", className)}
     >
       {Array(repeat)
         .fill(0)
         .map((_, i) => (
-          <div
-            key={i}
-            className={cn("flex shrink-0 justify-around [gap:var(--gap)]", {
-              "animate-marquee flex-row": !vertical,
-              "animate-marquee-vertical flex-col": vertical,
-              "group-hover:[animation-play-state:paused]": pauseOnHover,
-              "[animation-direction:reverse]": reverse
-            })}
-          >
+          <div key={i} className="ui-marquee__group">
             {children}
           </div>
         ))}
